f5fd2e50f6062b2631368a53433f730d5dc7055f
fad0805 <s222y@hotmail.com>
2025-03-25 07:12:12 +0900

    Remove one menu from navbar

--- front/components/navbar.tsx
+++ front/components/navbar.tsx
@@ -5,7 +5,6 @@ import "@/public/styles/navbar.scss";
 
 export const Navbar = () => {
     return <nav id="navbar">
-        <Link href="#">글쓴이</Link>
         <Link href="#">등장인물</Link>
         <Link href="#">책장</Link>
         <Link className="active" href="/">놀러가기</Link>
