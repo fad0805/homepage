9f36b9ce3f08abc91870e17240161333b063aadd
fad0805 <s222y@hotmail.com>
2025-06-09 08:00:49 +0900

    Add some photo to characters page

--- web/app/characters/page.tsx
+++ web/app/characters/page.tsx
@@ -24,19 +24,19 @@ export default function Characters() {
       characters: [
         {
           name: "루시안",
-          image: "#",
+          image: "https://objectstorage.ap-seoul-1.oraclecloud.com/n/cnftfbml9zxa/b/bucket-20230907-0739/o/misskey/fc355ca2-5cc9-42a2-b692-00c4d4803f2a.webp",
         },
         {
           name: "루시엘",
-          image: "#",
+          image: "https://objectstorage.ap-seoul-1.oraclecloud.com/n/cnftfbml9zxa/b/bucket-20230907-0739/o/misskey/635fd37e-b2f7-4b0f-84ef-1342175b19c3.webp",
         },
         {
           name: "루아란",
-          image: "#",
+          image: "https://objectstorage.ap-seoul-1.oraclecloud.com/n/cnftfbml9zxa/b/bucket-20230907-0739/o/misskey/2bf3b392-e5c1-46d3-a1a3-74bfe8631a98.webp",
         },
         {
           name: "루이린",
-          image: "#",
+          image: "https://objectstorage.ap-seoul-1.oraclecloud.com/n/cnftfbml9zxa/b/bucket-20230907-0739/o/misskey/3b12d7cc-e84e-495c-a96e-c2ead1b783c7.webp",
         },
         {
           name: "루 시",
