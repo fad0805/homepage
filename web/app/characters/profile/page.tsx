da9fe108edf32d4baa38486d05be2b751f1f7a4d
fad0805 <s222y@hotmail.com>
2025-06-11 19:07:17 +0900

    Remove useSearchParams

--- web/app/characters/profile/page.tsx
+++ web/app/characters/profile/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { useSearchParams } from 'next/navigation';
-import '@/public/styles/characters.scss';
-
 export default function Profile() {
-  const character = () => {
-    const searchParams = useSearchParams();
-    return searchParams.get('character');
-  }
-
   return (
     <div id="profile">
-      { character }
+      준비 중...
     </div>
   );
 }
