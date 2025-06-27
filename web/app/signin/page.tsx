eadbfae9b078ce6d82ea34c4ed28458d163665ce
fad0805 <s222y@hotmail.com>
2025-06-27 18:50:45 +0900

    Add submit handler in signin page

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -1,14 +1,21 @@
 "use client";
+import { FormEvent } from 'react';
 
 import '@/public/styles/signin.scss';
 
 export default function Home() {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+  };
+
   return (
     <div id="main">
-      <form action="/signin" method="post">
+      <form onSubmit={handleSubmit}>
         <h2>관리자 로그인</h2>
-        <div className="error"></div>
-        <div className="success"></div>
+        <div className="info"></div>
         <div className="inputs">
           <input type="text" id="username" name="username" placeholder="Username" required />
           <input type="password" id="password" name="password" placeholder="Password" required />
