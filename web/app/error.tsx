0cd5d71bd0cddbe9d898b8aa7c85fc94d49d190e
fad0805 <s222y@hotmail.com>
2025-07-19 13:51:03 +0900

    Add error and not found page

---
+++ web/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import '@/public/styles/error.scss';
+
+export default function Error() {
+  return (
+    <div id="error">
+      문제가 발생했습니다.
+    </div>
+  );
+}
---
+++ web/app/not-found.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import '@/public/styles/error.scss';
+
+export default function Error() {
+  return (
+    <div id="error-404">
+      404 - Page Not Found
+    </div>
+  );
+}
