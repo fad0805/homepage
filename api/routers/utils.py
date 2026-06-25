b6ec07828f0f0bc924a1c1a727a672a3e35f4da1
fad0805 <s222y@hotmail.com>
2026-06-25 13:31:11 +0900

    Create random SECRET_KEY when start development setting

--- api/routers/utils.py
+++ api/routers/utils.py
@@ -10,12 +10,16 @@ from passlib.context import CryptContext
 
 from crud.users import get_user
 
+ENVIRONMENT = os.getenv('ENVIRONMENT')
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM', 'HS256')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv('REFRESH_TOKEN_EXPIRE_DAYS', 7))
 
-if not SECRET_KEY:
+if ENVIRONMENT == 'development':
+    import secrets
+    SECRET_KEY = os.getenv('SECRET_KEY') or secrets.token_hex(32)
+elif not SECRET_KEY:
     raise ValueError("SECRET_KEY environment variable is not set.")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
